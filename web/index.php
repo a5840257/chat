c63484103bb7d513712f34f63d22e6a38b66e82a
LinYuHe <linyuhe@qq.com>
2017-12-13 14:36:33 +0800

    修改标题

--- web/index.php
+++ web/index.php
@@ -29,7 +29,7 @@ unset($userList[$uid]);
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>前端js(jquery)在线聊天</title>
+    <title>前端js1111(jquery)在线聊天</title>
     <meta name="keywords" content="前端js在线聊天"/>
     <meta name="description" content="前端js在线聊天"/>
 
